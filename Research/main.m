3384dc3d08fdc4ad790b453a58a26b9c81ab4f28
adityagoel <adityadecember25@gmail.com>
2014-04-09 15:04:07 -0700

    Initial Commit

---
+++ Research/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Research
+//
+//  Created by Aditya Goel on 4/9/14.
+//  Copyright (c) 2014 ADITYAGOEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ResearchTests/ResearchTests.m
@@ -0,0 +1,34 @@
+//
+//  ResearchTests.m
+//  ResearchTests
+//
+//  Created by Aditya Goel on 4/9/14.
+//  Copyright (c) 2014 ADITYAGOEL. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ResearchTests : XCTestCase
+
+@end
+
+@implementation ResearchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
